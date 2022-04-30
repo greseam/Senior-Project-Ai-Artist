b33e3f0c436bd338c398d338517fc4aa1db8112e
Gregor <greseam@dunwoody.edu>
2022-04-30 12:31:00 -0500

    ganCode

--- AiArtist.py
+++ AiArtist.py
@@ -7,15 +7,19 @@
 #
 #############################################
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import pickle
+import torch
+from torch_utils import *
+from dnnlib import *
+
 import sys
+
 
 #users -- using a dictionary to simplify workload, under real project restraints this would be a sql server of somekind
 users = {
-    "johnsmith": "1234abc" #bad password is bad i know, but used only for debugging and prototyping
+    "johnsmith": "1234abc" #bad password i know, but used only for debugging and prototyping
 }
 
 class UI(QMainWindow):
@@ -39,19 +43,49 @@ class UI(QMainWindow):
         self.genBttn = self.findChild(QPushButton, "pushButton")
         self.info = self.findChild(QPushButton, "info_bttn")
 
-        self.genBttn.clicked.connect(self.clicker)
+        self.genBttn.clicked.connect(self.generateImage)
 
         #show the UI
         self.show()
 
 
-    def clicker(self):
+    def generateImage(self):
+        
+        #!!Button disapear
+        #!!loading/progress bar
+        
+        
         #self.label.setText(f'Hello there {self.textedit.toPlainText()}') 
+
         seed = self.textedit.toPlainText()
+        
+        #put seed usage here
         print(seed)
+        
         self.textedit.setPlainText("")
 
 
+        #put img gen code here
+        pkl = "network-snapshot-000400.pkl"
+        pkl = f"./data/{pkl}"
+        with open(pkl, 'rb') as f:
+            G = pickle.load(f)['G_ema'].cuda()  # torch.nn.Module
+        z = torch.randn([1, G.z_dim]).cuda()    # latent codes
+        c = None                                # class labels (not used in this example)
+        img = G(z, c)                           # NCHW, float32, dynamic range [-1, +1]
+        
+        w = G.mapping(z, c, truncation_psi=0.5, truncation_cutoff=8)
+        img = G.synthesis(w, noise_mode='const', force_fp32=True)
+
+
+        #display to label_2 code here
+        self.im = QPixmap(img)
+        #self.im = QPixmap("./uiref/icon.jpg")
+        self.label.setPixmap(self.im)
+
+        #!!button becomes active
+
+    #select pickle
 
 
 #initalize app
